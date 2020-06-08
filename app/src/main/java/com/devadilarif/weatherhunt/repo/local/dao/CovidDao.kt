e525b74b27397885f360997a996f66ee3efe3467
avinash003 <55076731+avinash003@users.noreply.github.com>
2020-06-09 00:09:11 +0530

    CovidDao @Dev

--- app/src/main/java/com/devadilarif/weatherhunt/repo/local/dao/CovidDao.kt
+++ app/src/main/java/com/devadilarif/weatherhunt/repo/local/dao/CovidDao.kt
@@ -1,8 +1,22 @@
 package com.devadilarif.weatherhunt.repo.local.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import com.devadilarif.weatherhunt.repo.local.model.COVID19
+import io.reactivex.Single
 
 @Dao
 interface CovidDao {
+
+    @Insert
+    fun insertCovid19(covid19: COVID19)
+
+    @Delete
+    fun deleteCovid19(covid19: COVID19)
+
+    @Query("SELECT * FROM COVID19")
+    fun getCovidUpdates(): Single<COVID19>
 
 }
--- app/src/main/java/com/devadilarif/weatherhunt/repo/local/model/COVID19.kt
+++ app/src/main/java/com/devadilarif/weatherhunt/repo/local/model/COVID19.kt
@@ -5,19 +5,7 @@ import androidx.room.Entity
 
 @Entity
 data class COVID19(
-    val active_cases_critical_percentage: String,
-    val active_cases_mild_percentage: String,
-    val cases_with_outcome: String,
-    val closed_cases_death_percentage: String,
-    val closed_cases_recovered_percentage: String,
-    val critical_condition_active_cases: String,
-    val currently_infected: String,
     val death_cases: String,
-    val death_closed_cases: String,
-    val general_death_rate: String,
-    val last_update: String,
-    val mild_condition_active_cases: String,
-    val recovered_closed_cases: String,
     val recovery_cases: String,
     val total_cases: String
 )
