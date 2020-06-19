3dad98c4407060120e4228be710e75fec9150556
avinash003 <55076731+avinash003@users.noreply.github.com>
2020-06-19 15:31:02 +0530

    LocationPermissionFragment and StoragePermissionFragment :- UI Done @Dev

---
+++ app/src/main/java/com/devadilarif/weatherhunt/fragments/LocationPermissionFragment.kt
@@ -0,0 +1,22 @@
+package com.devadilarif.weatherhunt.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.devadilarif.weatherhunt.R
+
+class LocationPermissionFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_location_permission, container, false)
+    }
+
+
+}
