5db5c7bbc71d42a36797e8dcd7e8389db05524b0
avinash003 <55076731+avinash003@users.noreply.github.com>
2020-06-24 14:29:58 +0530

    Permission && Button && ScrollView && Circular Image View @Dev

--- app/src/main/java/com/devadilarif/weatherhunt/fragments/LocationPermissionFragment.kt
+++ app/src/main/java/com/devadilarif/weatherhunt/fragments/LocationPermissionFragment.kt
@@ -1,21 +1,28 @@
 package com.devadilarif.weatherhunt.fragments
 
 import android.Manifest
+import android.app.AlertDialog
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
+import android.widget.Button
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.content.ContextCompat.checkSelfPermission
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import com.devadilarif.weatherhunt.MainActivity
 import com.devadilarif.weatherhunt.R
+import kotlinx.android.synthetic.main.fragment_location_permission.*
+import kotlinx.android.synthetic.main.fragment_location_permission.view.*
+
 
 class LocationPermissionFragment : Fragment() {
-
-
     companion object{
         const val LOCATION_PERMISSION_RC = 1
     }
@@ -24,26 +31,26 @@ class LocationPermissionFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_location_permission, container, false)
+        val view: View  = inflater.inflate(R.layout.fragment_location_permission, container, false)
+        view.allowButton.setOnClickListener { askLocationPermission() }
+        return view
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        askLocationPermission()
-    }
 
     fun askLocationPermission(){
         if(checkSelfPermission(context!!,Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
-            ActivityCompat.requestPermissions(activity!!, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),LOCATION_PERMISSION_RC)
+            requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),LOCATION_PERMISSION_RC)
         }else{
             //move to another fragment
-            moveToStorageFragment()
+            moveToMainActivity()
         }
     }
 
-    fun moveToStorageFragment(){
-
+    fun moveToMainActivity(){
+        startActivity(Intent(context, MainActivity::class.java))
+        activity?.finish()
     }
+
 
     override fun onRequestPermissionsResult(
         requestCode: Int,
@@ -51,7 +58,7 @@ class LocationPermissionFragment : Fragment() {
         grantResults: IntArray
     ) {
         if(requestCode == LOCATION_PERMISSION_RC && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-            moveToStorageFragment()
+            moveToMainActivity()
         }else{
             Toast.makeText(context,resources.getString(R.string.location_permission_not_given),Toast.LENGTH_LONG).show()
         }
