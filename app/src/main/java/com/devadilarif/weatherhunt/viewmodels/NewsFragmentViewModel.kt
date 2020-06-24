fa47763533793eb88cae615e22cb962522d30086
mdadil2019 <mdadil2019@gmail.com>
2020-06-24 16:11:21 +0530

    News Fragment with viewmodel integration but not tested

--- app/src/main/java/com/devadilarif/weatherhunt/viewmodels/NewsFragmentViewModel.kt
+++ app/src/main/java/com/devadilarif/weatherhunt/viewmodels/NewsFragmentViewModel.kt
@@ -1,5 +1,7 @@
 package com.devadilarif.weatherhunt.viewmodels
 
+import androidx.databinding.ObservableField
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.devadilarif.weatherhunt.repo.NewsRepository
@@ -7,11 +9,25 @@ import com.devadilarif.weatherhunt.repo.local.model.News
 
 class NewsFragmentViewModel(private val newsRepository: NewsRepository) : ViewModel() {
 
-    private var news = MutableLiveData<List<News>>()
+    var news = ObservableField<News>()
 
+    private var newsList = arrayListOf<News>()
+    private var currrentPosition = 0
     init {
-        newsRepository.getTopHeadlines {
-            news.postValue(it)
+        newsRepository.getTopHeadlines { it ->
+            news.set(it.get(0))
+            it.forEach { newsList.add(it) }
         }
+    }
+
+    fun getNextNewsTitle() : String{
+        return if(currrentPosition != newsList.size - 1) newsList.get(currrentPosition + 1).title else newsList.get(currrentPosition).title
+    }
+
+    fun changeNews(){
+        currrentPosition++
+        if(currrentPosition == newsList.size - 1) currrentPosition = 0
+
+        news.set(newsList.get(currrentPosition))
     }
 }
