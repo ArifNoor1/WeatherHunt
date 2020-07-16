ab7a10c92322b21edae72d6499138f037880e741
avinash003 <55076731+avinash003@users.noreply.github.com>
2020-07-16 14:58:59 +0530

    WeatherForcastAdapter - Context in Adapter @Dev

--- app/src/main/java/com/devadilarif/weatherhunt/adapter/WeatherForecastAdapter.kt
+++ app/src/main/java/com/devadilarif/weatherhunt/adapter/WeatherForecastAdapter.kt
@@ -1,13 +1,18 @@
 package com.devadilarif.weatherhunt.adapter
 
+import android.app.Activity
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import com.devadilarif.weatherhunt.R
 import com.devadilarif.weatherhunt.databinding.WeatherItemBinding
+import com.devadilarif.weatherhunt.fragments.WeatherFragment
 import com.devadilarif.weatherhunt.repo.local.model.Forcast
-import com.devadilarif.weatherhunt.repo.local.model.ForecastResponse
+import kotlinx.android.synthetic.main.weather_item.view.*
+
 
 class WeatherForecastAdapter(val forecasts: List<Forcast>) :
     RecyclerView.Adapter<WeatherForecastAdapter.WeatherForcastViewHolder>() {
@@ -24,14 +29,44 @@ class WeatherForecastAdapter(val forecasts: List<Forcast>) :
 
     override fun onBindViewHolder(holder: WeatherForcastViewHolder, position: Int) {
         holder.onBind(forecasts[position])
+        holder.cardView.setOnClickListener {
+            holder.showWeatherFragment()
+        }
+
+
     }
 
     inner class WeatherForcastViewHolder(val weatherForcastDataBinding : WeatherItemBinding)
         : RecyclerView.ViewHolder(weatherForcastDataBinding.root) {
 
+
+        var cardView = weatherForcastDataBinding.root.card_view
+
         fun onBind(forcast : Forcast){
             weatherForcastDataBinding.weatherResponse = forcast
+
         }
+
+        fun showWeatherFragment(){
+//            weatherForcastDataBinding.root.
+            var activity = weatherForcastDataBinding.root.context as AppCompatActivity
+            val myFragment: Fragment = WeatherFragment()
+            activity.supportFragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainer, myFragment).addToBackStack(null).commit()
+
+        }
+
+
+        /*fun dataToFragment(forecast: Forcast, context: Context) {
+
+            val weatherFragment = WeatherFragment()
+            val args = Bundle()
+            args.putParcelable("DATA_FORECAST", forecast)
+            weatherFragment.arguments = args
+
+
+
+        }*/
     }
 
 
