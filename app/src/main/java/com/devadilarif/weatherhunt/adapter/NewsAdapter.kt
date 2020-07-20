39957284e58f3871941c021b14bbf51cd8640e16
avinash003 <55076731+avinash003@users.noreply.github.com>
2020-07-20 12:52:26 +0530

    WebView for news On HomeFragment and Settings Fragment @Dev

--- app/src/main/java/com/devadilarif/weatherhunt/adapter/NewsAdapter.kt
+++ app/src/main/java/com/devadilarif/weatherhunt/adapter/NewsAdapter.kt
@@ -1,12 +1,18 @@
 package com.devadilarif.weatherhunt.adapter
 
+import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.devadilarif.weatherhunt.R
 import com.devadilarif.weatherhunt.databinding.NewsItemBinding
+import com.devadilarif.weatherhunt.fragments.WebFragment
 import com.devadilarif.weatherhunt.repo.local.model.News
+import kotlinx.android.synthetic.main.news_item.view.*
+import kotlinx.android.synthetic.main.weather_item.view.*
+import kotlinx.android.synthetic.main.weather_item.view.card_view
 
 class NewsAdapter(val topHeadlines : List<News>)  : RecyclerView.Adapter<NewsAdapter.NewsItemViewHolder> (){
 
@@ -34,6 +40,20 @@ class NewsAdapter(val topHeadlines : List<News>)  : RecyclerView.Adapter<NewsAda
 
         fun onBind(news : News){
             newsDataBinding.news = news
+            newsDataBinding.root.card_view.setOnClickListener {
+                showWebFragment(news)
+            }
+
+        }
+
+        fun showWebFragment(news: News){
+            val fragment = WebFragment()
+            val activity = newsDataBinding.root.context as AppCompatActivity
+            val data =  Bundle()
+            data.putString("newsLink",news.url)
+            fragment.arguments = data
+            activity.supportFragmentManager.beginTransaction().add(R.id.fragmentContainer,fragment).addToBackStack("backStack").commit()
+
         }
     }
 }
